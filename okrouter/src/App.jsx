f7d2b88b9e58826ed4f24da0bc98606543dc8643
simair77 <85722851+simair77@users.noreply.github.com>
2022-07-12 17:47:14 +0900

    Update App.jsx

--- okrouter/src/App.jsx
+++ okrouter/src/App.jsx
@@ -4,7 +4,7 @@ function App(props) {
   const list = ['hello1', 'hello2', 'hello3', 'hello4', 'hello5'];
   return (
     <div className="App">
-      <h1>Main</h1>
+      <h1>Main 1</h1>
       {list.forEach((item, index) => (
         <Comp value={item} key={index} />
       ))}
